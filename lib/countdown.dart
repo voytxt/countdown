c3336d9446442a4fc3e460daae47626074e90678
Vojta <65811166+vojta-dev@users.noreply.github.com>
2022-08-26 12:21:31 +0200

    Cancel timer and show DONE when countdown ends

--- lib/countdown.dart
+++ lib/countdown.dart
@@ -23,10 +23,21 @@ class _CountdownState extends State<Countdown> {
     final arguments = ModalRoute.of(context)!.settings.arguments as CountdownArguments;
 
     Timer.periodic(const Duration(milliseconds: 100), (Timer timer) {
-      if (!mounted) return;
-      final displayNumber = ((arguments.end.millisecondsSinceEpoch - DateTime.now().millisecondsSinceEpoch) / 100).floor() / 10;
-      // TODO: End timer when countdown reaches 0
-      setState(() => _countdown = displayNumber >= 0 ? displayNumber.toString() : '0');
+      // Cancel the timer after we leave the countdown screen
+      if (!mounted) {
+        timer.cancel();
+        return;
+      }
+
+      final difference = arguments.end.millisecondsSinceEpoch - DateTime.now().millisecondsSinceEpoch;
+      final displayNumber = (difference / 100).floor() / 10;
+
+      if (displayNumber >= 0) {
+        setState(() => _countdown = displayNumber.toString());
+      } else {
+        setState(() => _countdown = 'DONE');
+        timer.cancel();
+      }
     });
 
     return Scaffold(
